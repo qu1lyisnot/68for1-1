fd8c54557fa3331ee349c17925a8ab585d1eac8b
qu1lyisnot <106623639+qu1lyisnot@users.noreply.github.com>
2022-08-20 22:09:32 +0600

    Create letterKeyGenerator.lua

---
+++ letterKeyGenerator.lua
@@ -0,0 +1,17 @@
+local keyGen = {}
+
+local function keyGen:GetKey()
+	local function generateLetter()
+
+		local randomBigChar = string.char(math.random(65,90)).. string.char(math.random(65,90)).. string.char(math.random(97, 122)).. string.char(math.random(65,90)).. string.char(math.random(97, 122)).. string.char(math.random(97, 122)).. string.char(math.random(97, 122))
+		local key = "null"
+		key = randomBigChar
+		return key
+	end
+	
+	local generatedKey = generateLetter().. generateLetter().. generateLetter().. generateLetter()
+	local token = ""
+	token = token.. generatedKey
+end
+
+return keyGen

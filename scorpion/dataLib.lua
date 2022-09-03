3027337a785876a58800cf4900a0329709452dc9
qu1lyisnot <106623639+qu1lyisnot@users.noreply.github.com>
2022-09-03 17:37:02 +0600

    Update dataLib.lua

--- scorpion/dataLib.lua
+++ scorpion/dataLib.lua
@@ -1,1 +1,28 @@
+local data = {}
 
+local premium = {}
+local blacklist = {}
+
+function data:GetPremiumUser()
+    local premiums = {
+        735060213, --Owner
+        569741870,
+        3589495030,
+        3185243666,
+        3121363344
+    }
+
+    table.insert(premium, premiums)
+    return premium
+end
+
+function data:GetBlacklistedUsers()
+    local blacklisted = {
+
+    }
+
+    table.insert(blacklist, blacklisted)
+    return blacklist
+end
+
+return data

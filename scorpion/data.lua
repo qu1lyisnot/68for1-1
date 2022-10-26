d28ae98d83bd0014e9fa80e08881ebd94414853b
qu1lyisnot <106623639+qu1lyisnot@users.noreply.github.com>
2022-10-26 22:10:52 +0600

    Create data.lua

---
+++ scorpion/data.lua
@@ -0,0 +1,19 @@
+local data = {}
+
+local premiumList = {
+  735060213,
+  3589495030
+}
+
+local blacklist = {
+}
+
+function data:GetPremiumList()
+  return premiumList
+end
+
+function data:GetBlacklist()
+  return blacklist
+end
+
+return data
